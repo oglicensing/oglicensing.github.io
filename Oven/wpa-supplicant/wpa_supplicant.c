ae8b38bd62cba229ff2546da38e7a85dae7f9e4f
i0rt <104755374+i0rt@users.noreply.github.com>
2022-12-01 10:38:09 +0000

    Deploying to gh-pages from @ oglicensing/oglicensing.github.io@c1a9025dbbce3d4cf3654d866718569641ea8a24 🚀

---
+++ Oven/wpa-supplicant/wpa_supplicant.c
@@ -0,0 +1,12 @@
+/*
+ * WPA Supplicant
+ * Copyright (c) 2003-2019, Jouni Malinen <j@w1.fi>
+ *
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
+ *
+ * This file implements functions for registering and unregistering
+ * %wpa_supplicant interfaces. In addition, this file contains number of
+ * functions for managing network connections.
+ */
+
