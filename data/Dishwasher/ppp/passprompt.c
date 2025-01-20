6d3a43632c0edf858ce5c373cde57e4dce6ee291
i0rt <i.ortega@onneragroup.com>
2025-01-20 10:43:47 +0100

    add dishwasher

---
+++ data/Dishwasher/ppp/passprompt.c
@@ -0,0 +1,10 @@
+/*
+ * passprompt.c - pppd plugin to invoke an external PAP password prompter
+ *
+ * Copyright 1999 Paul Mackerras, Alan Curry.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version
+ *  2 of the License, or (at your option) any later version.
+ */
---
+++ data/Dishwasher/qt5everywheredemo/main.cpp
@@ -0,0 +1,13 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:///qml/QtDemo/main.qml")));
+
+    return app.exec();
+}
